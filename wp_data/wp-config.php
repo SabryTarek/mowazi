1a91b9945504971d79555ae402ca2b9c8a98bb6e
Sabry Tarek <59545642+SabryTarek@users.noreply.github.com>
2022-06-09 11:47:46 +0200

    Update wp-config.php

--- wp_data/wp-config.php
+++ wp_data/wp-config.php
@@ -27,10 +27,10 @@ define( 'DB_NAME', 'mowazi');
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'admin' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
